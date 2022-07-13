52e1ec5207bf94c4cc01d4d6140101f651d23135
Tyler Nguyen <40559762+TylerXNguyen@users.noreply.github.com>
2022-07-13 16:56:10 -0700

    Updated result text on bottom
        Updates after each round who won it. Needs final result message
        when someone meets the rounds needed to win.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let player_wins = 0;
 let computer_wins = 0;
 let displayUserScore = document.querySelector(".user-score");
 let displayCPUScore = document.querySelector(".cpu-score");
+let displayResult = document.querySelector(".result");
+displayResult.querySelector(":scope > p");
 // Starts playing the rounds
 const imgSelections = document.querySelectorAll("img");
 imgSelections.forEach(selection => {
@@ -28,15 +30,18 @@ function playRound(playerSelection) {
     if (playerSelection == computerSelection) {
         // tie
         console.log("It's a tie. You both chose:\t" + playerSelection);
+        displayResult.textContent = "It's a tie. You both chose:\t" + playerSelection;
     } else if ((playerSelection == "Paper" && computerSelection == "Rock") || 
     (playerSelection == "Rock" && computerSelection == "Scissors") || 
     (playerSelection == "Scissors" && computerSelection == "Paper")) {
         // player wins
         console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        displayResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
         player_wins++;
     } else {
         // player loses
         console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        displayResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
         computer_wins++;
     }
     if (player_wins >= rounds || computer_wins >= rounds) {
