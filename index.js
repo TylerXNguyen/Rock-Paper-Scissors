ba464b77bac2eb0a60993a21651c848c91940d13
Tyler Nguyen <40559762+TylerXNguyen@users.noreply.github.com>
2022-07-13 17:09:23 -0700

    Added logic for winning game in amount of rounds
        Stopped the game when someone reached round limit.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // main
 let rounds = 1;
-//rounds = prompt("How many rounds to win?");
+rounds = prompt("How many rounds to win?");
 let player_wins = 0;
 let computer_wins = 0;
 let displayUserScore = document.querySelector(".user-score");
@@ -12,9 +12,6 @@ const imgSelections = document.querySelectorAll("img");
 imgSelections.forEach(selection => {
     selection.addEventListener("click", function () {
         playRound(selection.id);
-        // updates score
-        displayUserScore.textContent = player_wins;
-        displayCPUScore.textContent = computer_wins;
     });
 });
 
@@ -27,25 +24,35 @@ function computerPlay() {
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
     // checks for tie, same choice
-    if (playerSelection == computerSelection) {
-        // tie
-        console.log("It's a tie. You both chose:\t" + playerSelection);
-        displayResult.textContent = "It's a tie. You both chose:\t" + playerSelection;
-    } else if ((playerSelection == "Paper" && computerSelection == "Rock") || 
-    (playerSelection == "Rock" && computerSelection == "Scissors") || 
-    (playerSelection == "Scissors" && computerSelection == "Paper")) {
-        // player wins
-        console.log("You Win! " + playerSelection + " beats " + computerSelection);
-        displayResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
-        player_wins++;
-    } else {
-        // player loses
-        console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-        displayResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
-        computer_wins++;
+    if (player_wins < rounds && computer_wins < rounds) {
+        // while game is still playing
+        if (playerSelection == computerSelection) {
+            // tie
+            console.log("It's a tie. You both chose:\t" + playerSelection);
+            displayResult.textContent = "It's a tie. You both chose:\t" + playerSelection;
+        } else if ((playerSelection == "Paper" && computerSelection == "Rock") || 
+        (playerSelection == "Rock" && computerSelection == "Scissors") || 
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+            // player wins
+            console.log("You Win! " + playerSelection + " beats " + computerSelection);
+            displayResult.textContent = "You Win! " + playerSelection + " beats " + computerSelection;
+            player_wins++;
+        } else {
+            // player loses
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+            displayResult.textContent = "You Lose! " + computerSelection + " beats " + playerSelection;
+            computer_wins++;
+        }
+        displayUserScore.textContent = player_wins;
+        displayCPUScore.textContent = computer_wins;
     }
     if (player_wins >= rounds || computer_wins >= rounds) {
-        console.log("The Winner is");
+        console.log("The Game is over");
+        if (player_wins > computer_wins) {
+            displayResult.textContent = "Congratulations! You won the game.";
+        } else {
+            displayResult.textContent = "Sorry! The computer won the game.";
+        }
     }
     return 0;
 }
